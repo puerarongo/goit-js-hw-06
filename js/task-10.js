1edc044d5ba589a28b14bc64668b1cac2737439b
puerarongo <akhardin96@gmail.com>
2022-03-14 20:21:27 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,42 @@
+const inputEl = document.querySelector("input");
+const createEl = document.querySelector("button[data-create]");
+const destroyEl = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+let amount = null;
+let size = 20;
+
+
+// todo Function
+const inputFunc = (event) => {
+  amount = event.currentTarget.value;
+};
+
+const createBoxes = (event, amount) => {
+  for (let i = 1; i <= amount; i += 1) {
+    size += 10;
+    const markup = `<div style="width:${size}px; 
+    height:${size + 10}px; background:${getRandomHexColor()}"></div>`
+
+    boxesEl.insertAdjacentHTML("beforeend", markup);
+  }
+};
+
+const destroyBoxes = () => {
+  size = 20;
+  boxesEl.innerHTML = "";
+};
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+
+// todo Handler
+inputEl.addEventListener("input", inputFunc);
+createEl.addEventListener("click", (event) => createBoxes(event, amount));
+destroyEl.addEventListener("click", destroyBoxes);
+
